d75943d9dede9c8ae52b218bd075255b3403d5f1
Duong Tran <d@dttt.io>
2023-11-02 17:51:52 +0100

    1st commit

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Please set up conda env with the name "llava" and python 3.10 before running this script
+
+git clone https://github.com/haotian-liu/LLaVA.git
+cd LLaVA
+
+# conda create -n llava python=3.10 -y
+conda activate llava
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+
+# clone the repo but skip all LFS files (to avoid being stuck at "Filtering content")
+GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/liuhaotian/llava-v1.5-13b
+
+# download LFS files
+cd llava-v1.5-13b
+wget -N https://huggingface.co/liuhaotian/llava-v1.5-13b/resolve/main/mm_projector.bin
+wget -N https://huggingface.co/liuhaotian/llava-v1.5-13b/resolve/main/pytorch_model-00001-of-00003.bin
+wget -N https://huggingface.co/liuhaotian/llava-v1.5-13b/resolve/main/pytorch_model-00002-of-00003.bin
+wget -N https://huggingface.co/liuhaotian/llava-v1.5-13b/resolve/main/pytorch_model-00003-of-00003.bin
+wget -N https://huggingface.co/liuhaotian/llava-v1.5-13b/resolve/main/tokenizer.model
+
+# download eval.zip
+cd ../playground/data
+pip install --upgrade --no-cache-dir gdown
+ZIP_ID='1atZSBBrAX54yYpxtVVW33zFvcnaHeFPy'
+ZIP_NAME='eval.zip'
+gdown $ZIP_ID -O $ZIP_NAME
+unzip -q $ZIP_NAME
+rm $ZIP_NAME
+
+
+
